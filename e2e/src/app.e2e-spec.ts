66af0a2f82e055516a62c85f39a8b21592c218aa
serhii.klevchuk <serhii.klevchuk@gmail.com>
2019-12-24 07:53:22 +0200

    rename

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('drawing app is running!');
+    expect(page.getTitleText()).toEqual('game-of-life app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/drawing'),
+      dir: require('path').join(__dirname, './coverage/game-of-life'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
